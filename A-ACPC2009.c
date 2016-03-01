8052500e5d6866458ef41c62890f47dfae4f1940
Lekssays <lekssaysahmed@gmail.com>
2016-03-01 11:50:57 +0000

    Add comments

--- A-ACPC2009.c
+++ A-ACPC2009.c
@@ -7,14 +7,14 @@ Problem Name : [A] Seinfeld
 #include <string.h>
 #define MAX 2003
 
+// Implementing a stack using arrays
 struct stack {
 	char stk[MAX];
 	int top;
 } s;
 
 void push(char a){
-	if (s.top == MAX -1)
-		return;
+	if (s.top == MAX -1) return;
 	else {
 		s.top++;
 		s.stk[s.top] = a;
@@ -23,8 +23,7 @@ void push(char a){
 
 char pop(void){
 	char a;
-	if (s.top == -1)
-		return '-';
+	if (s.top == -1) return '-';
 	else {
 		a = s.stk[s.top];
 		s.top--;
@@ -35,27 +34,22 @@ char pop(void){
 int main(void){
 	s.top = -1;
 	char str[MAX];
-	int i,j,len, T, cntStack, total, k;
+	int i,j,len, cntStack, total, k = 1;
 
-	scanf("%d",&T);
-	k = 1;
-	while(str[0] != '-' && s.top == -1){
-		memset(str, 4, sizeof str);
-		cntStack = 0, len = 0; total = 0;
+	while(str[0] != '-'){
+		memset(str, 4, sizeof str); // set the array after every test case
+		cntStack = 0, total = 0; // initialize the value of the counters
 		scanf("%s",str);
 		len = strlen(str);	
 		for(i = 0; i < len ; i++){
 			if ( str[i] == '}' && s.top == -1){
 				push(str[i]);
-				total++;
-			} else if( str[i] == '}' && s.top != -1){
-				pop();
-			} else if( str[i] == '{'){
-				push(str[i]);
-			}			
+				total++; // add to operating -> we converted } to { because hte stack is empty
+			} else if( str[i] == '}' && s.top != -1) pop();
+			else push(str[i]);
 		}
 
-		while( s.top != -1){
+		while( s.top != -1){ // add the remaining { in the stack -> They ll always be even, so we devide by 2 to match every two pairs
 			pop();
 			cntStack++;
 		}
