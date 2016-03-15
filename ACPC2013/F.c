961f2ccdd212fe3862324bb06dc9851f2fdfc13b
Ahmed Lekssays <Lekssays@users.noreply.github.com>
2016-03-15 00:10:19 +0000

    Update F.c

--- ACPC2013/F.c
+++ ACPC2013/F.c
@@ -6,7 +6,7 @@ Problem: F - NASSA's Robot
 #include <string.h>
 #define MAX 100003
 
-int xmax , ymax, xmin, ymin, T, len, i, y , x, flag = 0;
+int T, len, i, y , x, cnt;
 char str[MAX];
 
 int main(void){
@@ -16,28 +16,17 @@ int main(void){
 		memset(str, 4, sizeof str);
 		scanf("%s",str);
 		len = strlen(str);
-		xmin = 0, xmax = 0, ymax = 0, ymin = 0, y = 0, x = 0;
+		cnt = 0, y = 0, x = 0;
 		for(i = 0; i < len; i++){
 			if( str[i] == 'U') y++;
 			else if( str[i] == 'D') y--;
 			else if( str[i] == 'R') x++;
 			else if ( str[i] == 'L') x--;
-		}
-		xmin = x, xmax = x, ymin = y, ymax = y;
-		for (i = 0; i < len; i++){
-			if ( str[i] == '?'){
-				ymax++;
-				ymin--;
-				xmax++;
-				xmin--;
-				flag = 1;
-			}
+			else cnt++;
 		}
 
-		if(flag == 1)
-			printf("%d %d %d %d\n",xmin, ymin, xmax, ymax);
-		else 
-			printf("%d %d %d %d\n",x,y,x,y);
+		printf("%d %d %d %d\n",x - cnt, y - cnt , x + cnt , y + cnt);
+
 	}
 	return 0;
 }
