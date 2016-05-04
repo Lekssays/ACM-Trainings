194863bd9d80f58280fe1cdee4ccb2c93483bd02
Ahmed Lekssays <Lekssays@users.noreply.github.com>
2016-05-04 22:24:38 +0100

    Update A.c

--- ACPC2014/A.c
+++ ACPC2014/A.c
@@ -2,37 +2,38 @@
 ACPC 2014
 Problem : A. Game of Peace
 */
-#include <stdio.h>
-#define size 100010
+#include <iostream>
+#define SIZE 100010
+
+using namespace std;
+
+long long fib[SIZE];
 
 long long gcd(long long a, long long b){
-	if(a > b)
-		return gcd( b, a);
-	if(a == 0)
-		return b;
-	else
-		return gcd(a, b % a);
+	if(a > b) return gcd(b , a);
+	if(a == 0) return b;
+	else return gcd(a, b % a);
 }
 
-int main (void){
-	long long t, i, m, n, x, y, a,b, fib[size], gcdA[size], j;
-	
-	scanf("%lld",&t);
+int main(void){
+	int T;
+	long long a, b, x, n, y, m;
+
+	cin >> T;
 
 	fib[0] = 0;
 	fib[1] = 1;
-	
-	for(i = 2, j = 0; i < size; i++, j++)
-		fib[i] = fib[i-1] + fib[i-2];
-		
-	for(i = 0; i < t; i++){
-		scanf("%lld %lld %lld %lld",&x,&n,&y,&m);
-		
-		a = fib[ n + 1 ] * x + y;
-		b = fib[ n ] * x;
-		
-		printf("Case %lld: %lld\n", i+1, gcd( a ,b ));
+
+	for(long long i = 2; i < SIZE; i++){
+		fib[i] = fib[i - 1] + fib[i - 2];
 	}
-	
-  return 0;
+
+	for(long long i = 0; i < T; i++){
+		cin >> x >> n >> y >> m;
+		a = fib[n + 1] * x + y;
+		b = fib[n] * x;
+		cout << "Case " << i + 1 << ": " << gcd(a, b) << endl;
+	}
+
+	return 0;
 }
